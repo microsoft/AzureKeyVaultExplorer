041ca0d6aa0e882c2b440faf3b1487906a6907c2
Eli Zeitlin <elize@microsoft.com>
2016-05-20 01:39:10 -0700

    Vault Explorer - adding support for drag & drop of secrets between apps

---
+++ Vault/Explorer/AddSecretEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.KeyVault;
+using System;
+
+namespace Microsoft.PS.Common.Vault.Explorer
+{
+    public class AddSecretEventArgs : EventArgs
+    {
+        public readonly Secret Secret;
+
+        public AddSecretEventArgs(Secret secret)
+        {
+            Secret = secret;
+        }
+    }
+}
+
