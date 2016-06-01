afe8e001a4b094d7fdaf6b1752ed17208b965146
Eli Zeitlin <elize@microsoft.com>
2016-05-31 23:16:49 -0700

    Vault Explorer - FormTelemetry for all forms with proper handling for AnyCPU inside Visual Studio WinForms designer

--- Vault/Explorer/PasswordDialog.cs
+++ Vault/Explorer/PasswordDialog.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Microsoft.PS.Common.Vault.Explorer
 {
-    public partial class PasswordDialog : Form
+    public partial class PasswordDialog : FormTelemetry
     {
         public PasswordDialog()
         {
