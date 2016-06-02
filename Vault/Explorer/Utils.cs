071fccbca44a6d309383a93caa9f513814f2639d
Eli Zeitlin <elize@microsoft.com>
2016-06-02 12:42:21 -0700

    Vault Explorer - generate new secured password and guid

--- Vault/Explorer/Utils.cs
+++ Vault/Explorer/Utils.cs
@@ -120,5 +120,25 @@ namespace Microsoft.PS.Common.Vault.Explorer
             catch { }
             return string.Format(string.Format("{0}{1}{2}", title, version, optionalPrefix));
         }
+
+        public static string NewSecurePassword()
+        {            
+            var UpperCharsSet = Enumerable.Range(65, 26).Select(i => (byte)i).ToArray(); // A..Z
+            var LowerCharsSet = Enumerable.Range(97, 26).Select(i => (byte)i).ToArray(); // a..z
+            var NumbersSet = Enumerable.Range(48, 10).Select(i => (byte)i).ToArray(); // 0..9
+            var SpecialCharsSet = new byte[] { 33, 35, 40, 41, 64 }; // !, #, (, ), @
+            var All = UpperCharsSet.Concat(LowerCharsSet).Concat(NumbersSet).Concat(SpecialCharsSet).ToArray();
+
+            var r = new Random();
+            int length = r.Next(32, 41);
+
+            var u = Enumerable.Range(0, 5).Select(i => UpperCharsSet[r.Next(0, UpperCharsSet.Length)]);
+            var l = Enumerable.Range(0, 1).Select(i => LowerCharsSet[r.Next(0, LowerCharsSet.Length)]);
+            var n = Enumerable.Range(0, 1).Select(i => NumbersSet[r.Next(0, NumbersSet.Length)]);
+            var s = Enumerable.Range(0, 4).Select(i => SpecialCharsSet[r.Next(0, SpecialCharsSet.Length)]);
+            var a = Enumerable.Range(0, length - 11).Select(i => All[r.Next(0, All.Length)]);
+
+            return Encoding.ASCII.GetString(u.Concat(l).Concat(n).Concat(s).Concat(a).Shuffle().ToArray());
+        }
     }
 }
