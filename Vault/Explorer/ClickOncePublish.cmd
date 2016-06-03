ffc6d991872466a7d3625634521f0b787929d9fe
Eli Zeitlin <elize@microsoft.com>
2016-06-03 00:49:11 -0700

    VE - ClickOnce is almost ready

--- Vault/Explorer/ClickOncePublish.cmd
+++ Vault/Explorer/ClickOncePublish.cmd
@@ -1,10 +1,31 @@
 @echo off
+rem ===============================================================================================
+rem Script to build and publish new VaultExplorer ClickOnce package
+rem
+rem Author: Eli Zeitlin (elize) 01/16/2013
+rem ===============================================================================================
+
 setlocal
 
-msbuild /v:minimal /fl /flp:verbosity=normal /m /p:Configuration=Release /t:rebuild;publish
+set ClickOnceInstallUpdateUrl=\\ELIZE-HP8540\C$\Temp\VaultExplorer\
+
+msbuild /v:minimal /fl /flp:verbosity=normal /m /p:Configuration=Release;ClickOnceInstallUpdateUrl=%ClickOnceInstallUpdateUrl% /t:rebuild;publish
 
 if %ERRORLEVEL% NEQ 0 (
     echo Error: Build break!
     exit /b 1
 )
 
+del .\bin\Release\app.publish\VaultExplorer.exe
+if %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to delete unnecessary file. This is not expected!
+    exit /b 1
+)
+
+xcopy .\bin\Release\app.publish %ClickOnceInstallUpdateUrl% /S /Y /Q
+if %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to copy from .\bin\Release\app.publish to %ClickOnceInstallUpdateUrl%
+    exit /b 1
+)
+      
+start %windir%\explorer.exe "%ClickOnceInstallUpdateUrl%Application Files\"
