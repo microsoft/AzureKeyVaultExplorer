fce05f870587232f6e2aa2fc3e48f33c7981957a
Eli Zeitlin <elize@microsoft.com>
2016-06-10 21:28:10 -0700

    VE - Creating release annotation in ApplicationInsights

--- Vault/Explorer/ClickOncePublish.cmd
+++ Vault/Explorer/ClickOncePublish.cmd
@@ -17,12 +17,23 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b 1
 )
 
+set AppId=afe75cd6-6516-4fd8-938a-a3c8516966e5
+set ApiKey=i3ff4xf3j7f1zq2mddrlls6zig88et1uqw7e602m
+set ReleaseFilePath=.\bin\Release\app.publish\VaultExplorer.exe
+powershell -ExecutionPolicy Unrestricted -Command "iex \"^& '%~dp0\CreateReleaseAnnotation.ps1' -applicationId '%AppId%' -apiKey '%ApiKey%' -releaseFilePath '%ReleaseFilePath%'\""
+
+if %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to create release annotation 
+    exit /b 1
+)
+
 del .\bin\Release\app.publish\VaultExplorer.exe
 if %ERRORLEVEL% NEQ 0 (
     echo Error: Failed to delete unnecessary file. This is not expected!
     exit /b 1
 )
 
+echo Copying files from .\bin\Release\app.publish to %ClickOnceInstallUpdateUrl%
 xcopy .\bin\Release\app.publish %ClickOnceInstallUpdateUrl% /S /Y /Q
 if %ERRORLEVEL% NEQ 0 (
     echo Error: Failed to copy from .\bin\Release\app.publish to %ClickOnceInstallUpdateUrl%
