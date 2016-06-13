9e9f146d93994edbfeb5bebbe032c132a32f61b1
Eli Zeitlin <elize@microsoft.com>
2016-06-13 10:52:41 -0700

    VE - secret dialog improving support for kv certificates

--- Vault/Explorer/PropertyObjectCertificate.cs
+++ Vault/Explorer/PropertyObjectCertificate.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,46 +14,49 @@ namespace Microsoft.PS.Common.Vault.Explorer
     /// </summary>
     public class PropertyObjectCertificate : PropertyObject
     {
-        CertificateBundle _certificate;
+        public readonly CertificateBundle CertificateBundle;
+        public readonly X509Certificate2 Certificate;
 
         [DisplayName("Thumbprint")]
         [Browsable(true)]
-        public string Thumbprint => _certificate.X5T;
+        public string Thumbprint => CertificateBundle.X5T;
 
         [DisplayName("Policy attributes")]
         [Browsable(true)]
         [ReadOnly(true)]
         [TypeConverter(typeof(ExpandableObjectConverter))]
-        public CertificatePolicyAttributes PolicyAttributes => _certificate.Policy.Attributes;
+        public CertificatePolicyAttributes PolicyAttributes => CertificateBundle.Policy.Attributes;
 
         [DisplayName("Issuer reference")]
         [Browsable(true)]
         [ReadOnly(true)]
-        public string IssuerReference => _certificate.Policy.IssuerReference?.Name;
+        public string IssuerReference => CertificateBundle.Policy.IssuerReference?.Name;
 
         [DisplayName("Key properties")]
         [Browsable(true)]
         [ReadOnly(true)]
         [TypeConverter(typeof(ExpandableObjectConverter))]
-        public KeyProperties KeyProperties => _certificate.Policy.KeyProperties;
+        public KeyProperties KeyProperties => CertificateBundle.Policy.KeyProperties;
 
         [DisplayName("Life time actions")]
         [Browsable(true)]
         [ReadOnly(true)]
         [TypeConverter(typeof(ExpandableObjectConverter))]
-        public LifetimeAction[] LifetimeActions => _certificate.Policy.LifetimeActions.ToArray();
+        public LifetimeAction[] LifetimeActions => CertificateBundle.Policy.LifetimeActions.ToArray();
 
         [DisplayName("Certificate properties")]
         [Browsable(true)]
         [ReadOnly(true)]
         [TypeConverter(typeof(ExpandableObjectConverter))]
-        public X509CertificateProperties X509CertificateProperties => _certificate.Policy.X509CertificateProperties;
+        public X509CertificateProperties X509CertificateProperties => CertificateBundle.Policy.X509CertificateProperties;
 
-        public PropertyObjectCertificate(CertificateBundle certificate, PropertyChangedEventHandler propertyChanged) :
-            base(certificate.Id, certificate.Tags, certificate.Attributes.Enabled, certificate.Attributes.Expires, certificate.Attributes.NotBefore, propertyChanged)
+        public PropertyObjectCertificate(CertificateBundle certificateBundle, X509Certificate2 certificate, PropertyChangedEventHandler propertyChanged) :
+            base(certificateBundle.Id, certificateBundle.Tags, certificateBundle.Attributes.Enabled, certificateBundle.Attributes.Expires, certificateBundle.Attributes.NotBefore, propertyChanged)
         {
-            _certificate = certificate;
+            CertificateBundle = certificateBundle;
+            Certificate = certificate;
             _contentType = ContentType.Pkcs12;
+            _value = "Click on the link below to view or install the certificate";
         }
 
         protected override IEnumerable<KeyValuePair<string, string>> GetCustomTags()
